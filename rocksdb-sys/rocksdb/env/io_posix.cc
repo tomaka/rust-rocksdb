37463b58c2f5405efdd5c07a217396a734baa20f
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-02-19 16:13:26 +0100

    Make it compile for Android

--- rocksdb-sys/rocksdb/env/io_posix.cc
+++ rocksdb-sys/rocksdb/env/io_posix.cc
@@ -156,7 +156,7 @@ Status PosixSequentialFile::Read(size_t n, Slice* result, char* scratch) {
   Status s;
   size_t r = 0;
   do {
-    r = fread_unlocked(scratch, 1, n, file_);
+    r = fread(scratch, 1, n, file_);
   } while (r == 0 && ferror(file_) && errno == EINTR);
   *result = Slice(scratch, r);
   if (r < n) {
